1fe28e65518c83b2a5ce19093115bdd4fe9ead71
David Fashbinder <david.fashbinder@gmail.com>
2020-12-19 19:50:35 -0500

    updated html

--- plots.js
+++ plots.js
@@ -89,6 +89,6 @@ function buildCharts(sample) {
       title: "Top 10 Bacteria Cultures Found"
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("plot", barData, barLayout);
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }
