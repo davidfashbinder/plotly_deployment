41db7c5e36e051c1a361461c587a7d69aa12ca9a
David Fashbinder <david.fashbinder@gmail.com>
2020-12-19 21:30:12 -0500

    adding bubble chart

--- plots.js
+++ plots.js
@@ -53,46 +53,77 @@ function buildMetadata(sample) {
   });
 }
 
-// 1. Create the buildCharts function.
-function buildCharts(sample) {
-  // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  // Create the buildChart function.
+  function buildCharts(sample) {
+    // Use d3.json to load the samples.json file
+    d3.json("samples.json").then((data) => {
       console.log(data);
-    // 3. Create a variable that holds the samples array. 
-    var samples = data.samples
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var person = samples.filter(val => val.id == sample);
-    //  5. Create a variable that holds the first sample in the array.
-    var firstPerson = person[0];
 
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otu_ids = firstPerson.otu_ids;
-    var otu_labels = firstPerson.otu_labels;
-    var sample_values = firstPerson.sample_values;
+      // Create a variable that holds the samples array.
+      var samples = data.samples;
+      // console.log(samples);
 
-    // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last.  
+      // Create a variable that filters the samples for the object with the desired sample number.
+      var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
 
-    var yticks = otu_ids.slice(0,10).map(otuID => 'OTU ${otuID}').reverse();
+      // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+      //var metadataArray = data.metadata.filter(sampleObj => sampleObj.id == sample);
 
-    // 8. Create the trace for the bar chart. 
-    var barData = [
-      {
-        x: sample_values.slice(0,10).reverse(),
-        text: otu_labels.slice(0, 10).reverse(),
-        y: yticks,
-        type: "bar",
-        orientation: "h",
-      }
-    ];
-    
+      // Create a variable that holds the first sample in the array.
+      var result = resultArray[0];
+
+      // 2. Create a variable that holds the first sample in the metadata array.
+      //var metadata = metadataArray[0];
+
+      // Create variables that hold the otu_ids, otu_labels, and sample_values.
+      var otu_ids = result.otu_ids;
+      var otu_labels = result.otu_labels;
+      var sample_values = result.sample_values;
+
+      // 3. Create a variable that holds the washing frequency.
+      //var frequency = parseFloat(metadata.wfreq);
+
+
+      // Create the yticks for the bar chart.
+      // Hint: Get the the top 10 otu_ids and map them in descending order
+      // so the otu_ids with the most bacteria are last.
+      var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+      // Create the trace for the bar chart.
+      var barData = [
+        {
+          y: yticks,
+          x: sample_values.slice(0, 10).reverse(),
+          text: otu_labels.slice(0, 10).reverse(),
+          type: "bar",
+          orientation: "h",
+        }
+      ];
+      // Create the layout for the bar chart.
+      var barLayout = {
+        title: "Top 10 Bacteria Cultures Found",
+        margin: {t: 30, l: 150}
+      };
+      // Use Plotly to plot the data with the layout.
+      Plotly.newPlot("bar", barData, barLayout);
+
+      var bubbleData = [
+        {
+          y: sample_values,
+          x: otu_ids,
+          text: otu_labels,
+          marker: {
+            size: sample_values,
+            color: otu_ids
+          }
+        }
+      ];
+
+      var bubbleLayout = {
+        title: "Bacteria Cultures Per Sample",
+        margin: {t: 30, l: 150}
+      };
       
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
-      title: "Top 10 Bacteria Cultures Found"
-    };
-    // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout);
-  });
-}
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    });
+  }
+
