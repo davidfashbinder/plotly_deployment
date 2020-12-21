1dc8964eb39ed6c25cbdcf3cbc247f9947f22551
David Fashbinder <david.fashbinder@gmail.com>
2020-12-20 20:16:03 -0500

    resize

--- plots.js
+++ plots.js
@@ -129,7 +129,7 @@ function buildMetadata(sample) {
       hovermode: "closest",
       margin: {l: 50, r: 50, b: 100, t: 100},
       autosize: false,
-      width: 500,
+      width: 1500,
       height: 500,
       xaxis: {
         title: 'OTU ID'
